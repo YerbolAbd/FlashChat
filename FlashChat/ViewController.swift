d0f646e1c68681be3647b1dec2c9d386e92980a5
YerbolAbd <abdiyevyerbol@yahoo.com>
2022-06-04 12:25:15 +0600

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Ербол on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
